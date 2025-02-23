1de766baf372665f97b20eaefe4c098106144a43
opsuperchain <opsuperchain@proton.me>
2025-02-23 15:35:31 -0500

    Exclude unused test files

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globalSetup: './test/integration/setup.ts',
-    setupFiles: []  // Remove setupFiles since we're using globalSetup
+    setupFiles: [],  // Remove setupFiles since we're using globalSetup
+    exclude: [
+      '**/lib/**/*.test.js',  // Exclude all test files in lib directories
+      '**/node_modules/**'    // Exclude all files in node_modules
+    ]
   }
 }) 
